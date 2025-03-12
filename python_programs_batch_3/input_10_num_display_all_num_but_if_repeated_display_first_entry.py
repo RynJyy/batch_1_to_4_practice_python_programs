1dd388ccf2e1fe69d929f9a3ca46393b7ef14052
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-12 19:31:59 +0800

    pseudocode prog3.3

--- python_programs_batch_3/input_10_num_display_all_num_but_if_repeated_display_first_entry.py
+++ python_programs_batch_3/input_10_num_display_all_num_but_if_repeated_display_first_entry.py
@@ -1,18 +1,15 @@
-# Create a list to store numbers
+# create a list to store numbers
 numbers = []
-
-# Ask the user to enter 10 numbers
+# input 10 numbers
 for i in range(10):
-    num = int(input(f"Enter number {i + 1}: "))
+    num = int(input(f"Enter the {i+1} number: "))
     numbers.append(num)
-
-# Display numbers (first entry only for duplicates)
-print("\nNumbers with no repeat entries are:")
+# check if the number is not repeated
 displayed_numbers = []
-  # List to track already displayed numbers
-
 for num in numbers:
     if num not in displayed_numbers:
         displayed_numbers.append(num)
+        # print the numbers make sure not to repeat the number
         print(num)
+
 
---
+++ python_programs_batch_3/input_num_until_user_input_invalid.py
@@ -0,0 +1,10 @@
+#create a list to store numbers
+#use while loop
+#input numbers until 
+#if number is not repeated display unique
+#if number is repeated display duplicate
+#stop when user input invalid number
+    
+    
+
+        
