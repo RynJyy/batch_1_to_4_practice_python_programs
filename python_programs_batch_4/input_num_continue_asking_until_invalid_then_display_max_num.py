a61bdb71b39f6b9d2fe4625067ebd49921f5b6c4
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-12 21:47:03 +0800

    final prog4.3

--- python_programs_batch_4/input_num_continue_asking_until_invalid_then_display_max_num.py
+++ python_programs_batch_4/input_num_continue_asking_until_invalid_then_display_max_num.py
@@ -1,5 +1,15 @@
 #create list
+numbers = []
 #use while loop
-#use try and except block
+while True:
+    #use try and except block
+    try:
+        num = int(input(f"Enter a number: "))
+        numbers.append(num)
+    except ValueError:
+        break
 #use max() function
-#display the highest number
+if numbers:
+    highest_num = max(numbers)
+    #display the highest number
+    print(f"\nThe highest number is: {highest_num}")
