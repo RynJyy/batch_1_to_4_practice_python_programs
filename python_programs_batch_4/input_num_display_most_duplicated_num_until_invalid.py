eb917d375e3986e326d3540ac1e0bfef18236073
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-12 21:33:46 +0800

    pseudocode prog4.2

---
+++ python_programs_batch_4/input_num_display_most_duplicated_num_until_invalid.py
@@ -0,0 +1,8 @@
+#create a list
+#use while loop 
+#use try and except block
+#input num
+#append the list with the numbers
+#check the most duplicated numbers in the list
+#display the most duplicated numbers
+#except block
