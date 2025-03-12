ca74f29d4cdf120ceb7aef85ef202b20a49731ac
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-12 21:30:44 +0800

    final prog4.1

--- python_programs_batch_4/input_10_num_display_duplicate.py
+++ python_programs_batch_4/input_10_num_display_duplicate.py
@@ -1,5 +1,14 @@
 #create a list
+numbers = []
 #use for loop and range to create a list of 10 numbers
-#append the list with the numbers 1 to 10
+for i in range (10):
+    num = int(input(f"Enter a number: "))
+    #append the list with the numbers 1 to 10
+    numbers.append(num)
 #check the duplicated numbers in the list
+duplicates = []
+for number in numbers:
+    if numbers.count(number) > 1:
+        duplicates.append(number)
 #display the duplicated numbers
+print(f"\nThe duplicated numbers are: {duplicates}")
