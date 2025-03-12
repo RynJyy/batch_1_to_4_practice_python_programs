3d97417c9cf89be9e7ecd1cb3d4456946ab2c0d2
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-12 17:30:01 +0800

    final prog2.&

--- python_programs_batch_2/input_10_num_print_num_of_even.py
+++ python_programs_batch_2/input_10_num_print_num_of_even.py
@@ -1,3 +1,9 @@
 #use for loops and range 
-#identify the even numbers
+even_counter = 0
+for i in range(10):
+    num = int(input(f"Enter the {i+1} number: "))
+    #identify the even numbers
+    if num % 2 == 0:
+        even_counter += 1
 #print how many even numbers
+print(f"\nThere are {even_counter} even numbers")
