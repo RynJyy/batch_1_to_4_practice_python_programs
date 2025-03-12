32a40928ccea37e6a84644fb123456b85d9284f6
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-12 14:52:16 +0800

    print output

--- python_programs_batch_1/input_2_num_add_all_print_sum.py
+++ python_programs_batch_1/input_2_num_add_all_print_sum.py
@@ -1,3 +1,7 @@
 #Ask 2 numbers
+num_1 = int(input("\nEnter the first number: "))
+num_2 = int(input("Enter the second number: "))
 #Add all numbers
-    #Print the sum 
+sum = num_1 + num_2
+#Print the sum 
+print(f"\nThe sum of the numbers is {sum}")
