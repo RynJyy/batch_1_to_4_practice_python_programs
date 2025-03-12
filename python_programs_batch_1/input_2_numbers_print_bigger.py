37abe6e214069fc73cc83d38dffae415be2166cf
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-12 14:33:10 +0800

    Print output

--- python_programs_batch_1/input_2_numbers_print_bigger.py
+++ python_programs_batch_1/input_2_numbers_print_bigger.py
@@ -2,5 +2,12 @@
 num_1 = int(input("\nEnter the first number: "))
 num_2 = int(input("Enter the second number: "))
 #Check the bigger number
+if num_1 > num_2:
     #Print the bigger number
-    
+    print(f"\nThe bigger number is: {num_1}")
+elif num_2 > num_1:
+    #Print the bigger number
+    print(f"\nThe bigger number is: {num_2}")
+else:
+    #if bot numbers are equal
+    print("\nInvalid")
