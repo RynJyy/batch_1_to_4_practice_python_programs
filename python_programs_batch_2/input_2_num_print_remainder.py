ed6b91fe8bfcec07313e7c339bdbaa42dfcab4b0
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-12 17:06:14 +0800

    final prog2.5

--- python_programs_batch_2/input_2_num_print_remainder.py
+++ python_programs_batch_2/input_2_num_print_remainder.py
@@ -1,5 +1,13 @@
 #Ask 2 numbers
+num_01 = int(input("\nEnter the first number: "))
+num_02 = int(input("Enter the second number: "))
 #Check if num_02 is not equal to 0
+if num_02 != 0:
     #Get the remainder
+    remainder = num_01 % num_02
     #Print the remainder
+    print(f"\nThe remainder is {remainder}")
 #If num_02 is equal to 0
+else:
+    print("\nInvalid")
+    
