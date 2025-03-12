efb62ad6240c67f7e3e3c692a4ab058706a6ebc9
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-12 15:20:01 +0800

    print output

---
+++ python_programs_batch_1/input_2_num_print_result_first_num_raised_to_second_num.py
@@ -0,0 +1,7 @@
+#Ask 2 numbers
+num_1 = int(input("\nEnter the first number: "))
+num_2 = int(input("Enter the second number: "))
+#Raised num 1 to the power of num 2
+result = num_1 ** num_2
+#Print the result
+print(f"\nThe {num_1} raised to the power of {num_2} is {result}")
