e1197e4791d5b9fd57605b2eb040b8e54c11766a
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-12 22:07:22 +0800

    final prog4.5

--- python_programs_batch_4/input_num_until_invalid_display_average.py
+++ python_programs_batch_4/input_num_until_invalid_display_average.py
@@ -1,5 +1,15 @@
 #create list
-#use while loop
-#use try and except block
+numbers = []
+#use while 
+while True:
+    #use try and except block
+    try:
+        num = int(input(f"Enter a number: "))
+        numbers.append(num)
+    except ValueError:
+        break
 #get the average of the numbers
+total = sum(numbers)
+average = total / len(numbers)
 #display the average
+print(f"\nThe average of the numbers is: {average:.2f}")
