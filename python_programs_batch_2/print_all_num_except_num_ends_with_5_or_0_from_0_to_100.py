268959dcbcd4e2bd7aeb11234c6e5dd1d0e787c9
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-12 17:52:13 +0800

    final prog2.9

--- python_programs_batch_2/print_all_num_except_num_ends_with_5_or_0_from_0_to_100.py
+++ python_programs_batch_2/print_all_num_except_num_ends_with_5_or_0_from_0_to_100.py
@@ -1,3 +1,8 @@
 #use for loops and range
-#check if num ends with 5 or 0
-#print result
+for i in range(101):
+    #check if num ends with 5 or 0
+    if i % 10 != 0 and i % 10 != 5:
+        #print result
+        print(i)
+
+    
