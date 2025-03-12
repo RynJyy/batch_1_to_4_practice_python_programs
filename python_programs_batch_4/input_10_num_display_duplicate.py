26ca218b756c326b6ae20d62e34bb2526102dfb7
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-12 21:20:01 +0800

    pseudocode prog4.1

---
+++ python_programs_batch_4/input_10_num_display_duplicate.py
@@ -0,0 +1,5 @@
+#create a list
+#use for loop and range to create a list of 10 numbers
+#append the list with the numbers 1 to 10
+#check the duplicated numbers in the list
+#display the duplicated numbers
