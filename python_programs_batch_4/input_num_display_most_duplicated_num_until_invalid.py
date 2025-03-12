277dfdb367bdb273092974ca1643c87be34bc98c
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-12 21:42:10 +0800

    final prog4.2

--- python_programs_batch_4/input_num_display_most_duplicated_num_until_invalid.py
+++ python_programs_batch_4/input_num_display_most_duplicated_num_until_invalid.py
@@ -1,8 +1,20 @@
 #create a list
+numbers = []
 #use while loop 
-#use try and except block
-#input num
-#append the list with the numbers
-#check the most duplicated numbers in the list
-#display the most duplicated numbers
-#except block
+while True:
+    try:
+        num = int(input(f"Enter a number: "))
+        #append the list with the numbers
+        numbers.append(num)
+    except ValueError:
+        break
+#check the duplicated numbers in the list
+if numbers:
+    most_duplicated = max(numbers, key = numbers.count)
+    #display the most duplicated numbers
+    print(f"\nThe most duplicated number is: {most_duplicated}")
+else:
+    print("No numbers entered.")
+
+    
+
