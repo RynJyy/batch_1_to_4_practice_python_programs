277f7a82b3b6ffb29d790d5f4609f98665bd3bfc
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-12 18:15:46 +0800

    final prog2.10

--- python_programs_batch_2/input_2_num_print_num_between.py
+++ python_programs_batch_2/input_2_num_print_num_between.py
@@ -1,4 +1,12 @@
 #Ask 2 numbers
+num_1 = int(input("\nEnter the first number: "))
+num_2 = int(input("Enter the second number: "))
 #Swap numbers if needed
+if num_1 > num_2:
+    num_1, num_2 = num_2, num_1
 #Print numbers in between
+for i in range(num_1+1, num_2):
+    print(i)
 #Check for no numbers in between
+if num_1+1 == num_2 or num_1 == num_2:
+    print("\nNo numbers in between")
