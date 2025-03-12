6a1d4a2b01df89de761ceb44dbb61637fd130307
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-12 19:47:48 +0800

    final prog3.4

--- python_programs_batch_3/input_num_continue_until_invalid_display_lowest_num.py
+++ python_programs_batch_3/input_num_continue_until_invalid_display_lowest_num.py
@@ -1,4 +1,14 @@
 #create list to store numbers
-#use whule loop
-#display the lowest number
-#continue until user input invalid number
+numbers = []
+#use while loop
+while True:
+    try:
+        #input numbers
+        num = int(input("Enter the number: "))
+        numbers.append(num)
+        #display the lowest number
+        print(min(numbers))
+    #continue until user input invalid number
+    except ValueError:
+        print("invalid")
+        break
