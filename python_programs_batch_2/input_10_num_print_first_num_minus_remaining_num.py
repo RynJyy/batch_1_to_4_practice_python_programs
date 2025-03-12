2d340c4ce49f748dfb6f9da30c7bd78c0d5adb03
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-12 17:23:19 +0800

    final prog2.6

--- python_programs_batch_2/input_10_num_print_first_num_minus_remaining_num.py
+++ python_programs_batch_2/input_10_num_print_first_num_minus_remaining_num.py
@@ -1,5 +1,10 @@
 #input num_1
+num_1 = int(input("\nEnter the 1 number: "))
 #use for loop and range for num_2 to num_10
-#Add the num_2 to num_10
+for i in range(9):
+    #Add the num_2 to num_10
+    sum = int(input(f"Enter the {i+2} number: "))
 #minus the sum to num_1
-#print result
+    num_1 -= sum
+#print the result
+print(f"\nThe result is {num_1}")
