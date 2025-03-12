d24c2dbec96b59a77f1630ffcb2eca5ac479caf2
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-12 15:35:38 +0800

    print output

--- python_programs_batch_1/input_10_num_add_all.py
+++ python_programs_batch_1/input_10_num_add_all.py
@@ -1,4 +1,9 @@
 #Use for loop and range
-#Ask user for a number
-#Sum of the 10 num
+sum = 0
+for i in range(10):
+    #Ask user for a number
+    num = int(input(f"Enter number {i + 1}: "))
+    #Sum of the 10 num
+    sum += num
 #Print the sum
+print(f"\nThe sum of the 10 numbers is {sum}")
