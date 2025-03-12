21d750f07dc3cb5e5b1339ffd4579233e9a04413
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-12 15:55:07 +0800

    print output

--- python_programs_batch_1/input_2_num_multiply_print_product.py
+++ python_programs_batch_1/input_2_num_multiply_print_product.py
@@ -1,4 +1,8 @@
 #Ask 2 numbers
+num_1 = int(input("\nEnter the first number: "))
+num_2 = int(input("Enter the second number: "))
 #Multiply all numbers
+sum = num_1 * num_2 
 #Print the product of the numbers
+print(f"\nThe product of the numbers is {sum}")
 
