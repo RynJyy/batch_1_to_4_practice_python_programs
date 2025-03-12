f4ce1d947cf8108899f5f0d694f73de3a86d7047
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-12 14:45:37 +0800

    print output

---
+++ python_programs_batch_1/input_2_numbers_print_if_equal.py
@@ -0,0 +1,9 @@
+#Ask 2 numbers
+num_1 = int(input("\nEnter the first number: "))
+num_2 = int(input("Enter the second number: "))
+#Check if equal
+if num_1 == num_2:
+    #Print if equal
+    print(f"\nThe numbers are equal {num_1}")
+else:
+    print("\nInvalid")
