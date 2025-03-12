45869c79e46c6e7f0a7f654d5587c99416f10c49
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-12 15:10:30 +0800

    print output

--- python_programs_batch_1/input_2_num_print_quotient_no_decimal.py
+++ python_programs_batch_1/input_2_num_print_quotient_no_decimal.py
@@ -1,4 +1,11 @@
 #Ask 2 numbers
+num_1 = int(input("\nEnter the first number: "))
+num_2 = int(input("Enter the second number: "))
 #Check if num 2 is not 0
-#Get the quotient
-#Print the quotient
+if num_2 != 0:
+    #Get the quotient
+    quotient = num_1 / num_2
+    #Print the quotient
+    print(f"\nThe quotient of the numbers is {quotient:.2f}")
+else:
+    print("\nInvalid")
