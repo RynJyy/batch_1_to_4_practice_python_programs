9b41fb2d465cf0824b4804787bb802b66e3b02d3
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-12 19:55:53 +0800

    final prog3.2

--- python_programs_batch_3/input_10_num_display_all_num_but_if_repeated_display_first_entry.py
+++ python_programs_batch_3/input_10_num_display_all_num_but_if_repeated_display_first_entry.py
@@ -1,5 +1,14 @@
 # create a list to store numbers
+numbers = []
 # input 10 numbers
+for i in range(10):
+    num = int(input(f"Enter the {i+1} number: "))
+    numbers.append(num)
 # check if the number is not repeated
+displayed_numbers = []
+for num in numbers:
+    if num not in displayed_numbers:
+        displayed_numbers.append(num)
         # print the numbers make sure not to repeat the number
+        print(num)
 
