dd4aaffc2078cfd3792d3d86dac37f64e73ed39c
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-12 22:00:54 +0800

    final prog4.4

--- python_programs_batch_4/input_num_until_invalid_then_display_num_highest_to_lowest.py
+++ python_programs_batch_4/input_num_until_invalid_then_display_num_highest_to_lowest.py
@@ -1,5 +1,14 @@
 #create list
+numbers = []
 #use while loop
+while True:
     #use try and except
+    try:
+        num = int(input(f"Enter a number: "))
+        numbers.append(num)
+    except ValueError:
+        break
 #use sort function to sort the list
+numbers.sort(reverse = True)
 #display the numbers in descending order
+print(f"\nThe numbers in descending order are: {numbers}")
