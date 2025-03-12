5296cf7e4b4f06d5adf00826f1a1ece80c542478
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-12 18:53:09 +0800

    final prog3.1

--- python_programs_batch_3/input_10_num_display_num_that_dont_have_duplicate.py
+++ python_programs_batch_3/input_10_num_display_num_that_dont_have_duplicate.py
@@ -1,4 +1,14 @@
 #Create a list
+numbers = []
 #Ask 10 numbers
+for i in range(10):
+    num = int(input(f"Enter the {i+1} number: "))
+    numbers.append(num)
 #Identify if numbers dont have duplicate
-#print the numbers that dont have duplicate
+unique_numbers = 0
+for num in numbers:
+    if numbers.count(num) == 1:
+        unique_numbers += 1
+        #print the numbers that dont have duplicate
+        print(f"\n The numbers that dont have duplicate:{num}")
+
