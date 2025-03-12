847f8e982be98bd10033571a160b4c568712f9b6
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-12 16:50:14 +0800

    final prog2.3

--- python_programs_batch_2/input_2_num_print_difference.py
+++ python_programs_batch_2/input_2_num_print_difference.py
@@ -3,5 +3,7 @@ num_1 = int(input("\nEnter the first number: "))
 num_2 = int(input("Enter the second number: "))
 #Know the difference
 difference = num_1 - num_2
+#Make it into absolute value
+difference = abs(difference)
 #Print the difference
-print(f"\nThe difference is {abs(difference)}")
+print(f"\nThe difference is {difference}")
