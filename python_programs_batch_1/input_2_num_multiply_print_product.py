b39e78e21b511fdfecad320af4695a268825fca3
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-12 15:49:56 +0800

    pseudocode 1.3

--- python_programs_batch_1/input_2_num_multiply_print_product.py
+++ python_programs_batch_1/input_2_num_multiply_print_product.py
@@ -1,7 +1,4 @@
 #Ask 2 numbers
-num_1 = int(input("\nEnter the first number: "))
-num_2 = int(input("Enter the second number: "))
 #Multiply all numbers
-product = num_1 * num_2
 #Print the product
-print(f"\nThe product of the numbers is {product}")
+
