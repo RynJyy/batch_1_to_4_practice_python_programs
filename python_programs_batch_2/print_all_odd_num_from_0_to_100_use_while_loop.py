f39e1885c76d232decf6d1c78ecad6aee0773795
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-12 17:39:47 +0800

    final prog2.8

--- python_programs_batch_2/print_all_odd_num_from_0_to_100_use_while_loop.py
+++ python_programs_batch_2/print_all_odd_num_from_0_to_100_use_while_loop.py
@@ -1,4 +1,9 @@
 #Use while loop to print all odd numbers from 0 to 100
-#check if num is odd
-#print result
-#increse num by 1 until 100
+num = 0
+while num <= 100:
+    #check if num is odd
+    if num % 2 != 0:
+        #print result
+        print(num)
+    #increse num by 1 until 100
+    num += 1
