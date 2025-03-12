eb39265104eda53479acbfa6e880c19ef71cadba
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-12 16:55:30 +0800

    final prog2.4

--- python_programs_batch_2/input_2_num_print_quotient_without_decimal.py
+++ python_programs_batch_2/input_2_num_print_quotient_without_decimal.py
@@ -1,3 +1,12 @@
 #Ask 2 numbers
-#Get the quotient without decimal
-#Print the quotient
+num_01 = int(input("\nEnter the first number: "))
+num_02 = int(input("Enter the second number: "))
+#Check if num_02 is not equal to 0
+if num_02 != 0:
+    #Get the quotient without decimal
+    quotient = num_01 // num_02
+    #Print the quotient
+    print(f"\nThe quotient without decimal is {quotient}")
+#If num_02 is equal to 0
+else:
+    print("\nInvalid")
