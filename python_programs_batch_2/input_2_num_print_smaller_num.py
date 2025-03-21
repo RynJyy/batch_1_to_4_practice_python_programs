00be4859c451a59a09011de3fe15a98dc2f4d359
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-21 17:52:09 +0800

    updated final prog2.1

--- python_programs_batch_2/input_2_num_print_smaller_num.py
+++ python_programs_batch_2/input_2_num_print_smaller_num.py
@@ -1,3 +1,11 @@
 # input num
+num_1 = int(input("Enter a number: "))
+num_2 = int(input("Enter a number: "))
 # check smaller num
-# print num
+if num_1 < num_2:
+    print(num_1) # print num
+elif num_2 < num_1:
+    print(num_2) # print num
+else:
+    print("invalid")
+
