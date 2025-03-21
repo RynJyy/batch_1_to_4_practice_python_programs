8a0915c6136da628bc0d18b4f8bc96d6ce22a617
Ryan Jay Canlas <ryancanlas299@gmail.com>
2025-03-21 17:47:16 +0800

    update pseudocode prog2.1

--- python_programs_batch_2/input_2_num_print_smaller_num.py
+++ python_programs_batch_2/input_2_num_print_smaller_num.py
@@ -1,15 +1,3 @@
-# Create a list to store numbers
-numbers = []
-
-# Ask the user to enter 10 numbers
-for i in range(10):
-    num = int(input(f"Enter number {i + 1}: "))
-    numbers.append(num)
-
-# Display numbers (first entry only for duplicates)
-displayed_numbers = []  # List to track already displayed numbers
-
-for num in numbers:
-    if num not in displayed_numbers:
-        print(num)
-        displayed_numbers.append(num)
+# input num
+# check smaller num
+# print num
